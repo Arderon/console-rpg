db94db614fcc936b323f7956e22abe474124ad88
hexlem <derpakarsen@gmail.com>
2026-06-25 09:31:33 +0300

    Create GameManager. Add name chosing

---
+++ ConsoleRPG/ConsoleRPG/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleRPG
+{
+    public class GameManager
+    {
+        public string Name { get; private set; }
+
+        public void StartGame()
+        {
+            Console.WriteLine("New game started. A new beginnig...");
+            CreateCharacter();
+        }
+
+        public void CreateCharacter()
+        {
+            ChooseName();
+            Console.WriteLine($"SI Arsen The Creator of ConsoleRPG declare that you are now {Name}");
+        }
+
+        private void ChooseName()
+        {
+            Console.WriteLine("Choose a name for your character: ");
+            string? name = "";
+            while (string.IsNullOrEmpty(name))
+            {
+                name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Name can`t be empty!");
+                    Console.WriteLine("Name: ");
+                }
+            }
+
+            Name = name;
+        }
+    }
+}
--- ConsoleRPG/ConsoleRPG/Main.cs
+++ ConsoleRPG/ConsoleRPG/Main.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ConsoleRPG
+{
+    class Program()
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ConsoleRPG!");
+            GameManager gm = new GameManager();
+            gm.StartGame();
+        }
+    }
+}
